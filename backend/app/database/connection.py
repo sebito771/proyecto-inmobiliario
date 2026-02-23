9be5d0a5d297958b1835db74e0b5339e149d7a4d
Sebastian <sebascova18@gmail.com>
2026-02-22 20:50:37 -0500

    feat(app/database): añade conexion a la base de datos

---
+++ backend/app/database/connection.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import settings
+
+# 1. Asegurar el protocolo correcto para MySQL
+url = settings.DATABASE_URL 
+DATABASE_URL = os.getenv("DATABASE_URL", url)
+
+# 2. Engine con Pool de Conexiones
+engine = create_engine(
+    DATABASE_URL,
+    pool_recycle=3600,  # Recicla conexiones cada hora para evitar cortes de MySQL
+    pool_pre_ping=True,  # Verifica si la conexión está viva antes de usarla (MUY RECOMENDADO)
+    echo=False           # Cambia a True si quieres ver el SQL real en consola (debug)
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
