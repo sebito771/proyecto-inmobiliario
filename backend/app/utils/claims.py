020f268917f87804e917af53e7818ebf553cb653
Sebastian <sebascova18@gmail.com>
2026-02-26 17:28:05 -0500

    feat(utils): añade funciones helper para mapear los datos del usuario a un diccionario

---
+++ backend/app/utils/claims.py
@@ -0,0 +1,24 @@
+from app.models.usuario import Usuario
+
+
+def get_claims(db_usuario: Usuario)-> dict:
+    """
+    Genera los claims para un token de acceso.
+    """
+    return {
+        "sub": str(db_usuario.id),
+        "email": db_usuario.email,
+        "rol_id": db_usuario.rol_id,
+        "type": "access",
+    }
+
+
+def get_verification_claims(db_usuario: Usuario) -> dict:
+    """
+    Genera los claims para un token de verificación de email.
+    """
+    return {
+        "sub": str(db_usuario.id),
+        "email": db_usuario.email,
+        "type": "verification",
+    }
