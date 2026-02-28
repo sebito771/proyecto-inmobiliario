ac6791e26df1c21e39cd404c2c93d22166bb0519
Sebastian <sebascova18@gmail.com>
2026-02-28 16:06:48 -0500

    feat(repo): añade metodo en repo compra para traer compras expiradas

--- backend/app/repo/compra.py
+++ backend/app/repo/compra.py
@@ -4,3 +4,10 @@ from app.models.compra import Compra as CompraModel
 class CompraRepository(BaseRepository):
     def __init__(self, db):
         super().__init__(CompraModel, db)
+
+    def get_expired_active(self, fecha_expiracion):
+        return self.db.query(CompraModel).filter(
+            CompraModel.fecha_expiracion < fecha_expiracion,
+            CompraModel.estado == "Activa"
+        ).all()
+
