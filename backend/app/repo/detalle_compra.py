e42a2d99bbc0cfebf6d8eb5a4b0f58826a99aed9
Sebastian <sebascova18@gmail.com>
2026-02-28 16:19:08 -0500

    feat(repo): añade metodo en detalle repo para buscar detalles relacionados con compra

--- backend/app/repo/detalle_compra.py
+++ backend/app/repo/detalle_compra.py
@@ -6,3 +6,7 @@ from app.models.detalle_compra import DetalleCompra as DetalleModel
 class DetalleRepository(BaseRepository):
     def __init__(self,db):
         super().__init__(DetalleModel, db)
+
+    
+    def get_by_compra_id(self,compra_id):
+        return self.db.query(DetalleModel).filter(DetalleModel.compra_id == compra_id).all()
