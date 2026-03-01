0a3f9842e2307afd225e0819cd5b9e60e78c413f
Sebastian <sebascova18@gmail.com>
2026-03-01 14:55:56 -0500

    fix(core): añade variables de entorno faltantes a la clase Settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,30 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
 
 class Settings(BaseSettings):
+    # Database
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    class Config:
-        env_file = ".env"
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+
+    # Email
+    MAIL_PASSWORD: str
+    MAIL_USERNAME: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+
+    model_config = ConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
 
 settings = Settings()
