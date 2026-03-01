d3a2382378690469896ae7b4e6b9477affa78b13
Sebastian <sebascova18@gmail.com>
2026-03-01 16:11:07 -0500

    fix(app/main): comenta rutas inactivas y sus imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from app.api.routes import auth,usuarios, lotes, compras, pagos, pqrs
+from app.api.routes import auth 
+# ,usuarios, lotes, compras, pagos, pqrs
 
 app = FastAPI()
 #active
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 #inactive
-app.include_router(usuarios.router, prefix="/usuarios", tags=["usuarios"])
-app.include_router(lotes.router, prefix="/lotes", tags=["lotes"])
-app.include_router(compras.router, prefix="/compras", tags=["compras"])
-app.include_router(pagos.router, prefix="/pagos", tags=["pagos"])
-app.include_router(pqrs.router, prefix="/pqrs", tags=["pqrs"])
+#app.include_router(usuarios.router, prefix="/usuarios", tags=["usuarios"])
+# app.include_router(lotes.router, prefix="/lotes", tags=["lotes"])
+# app.include_router(compras.router, prefix="/compras", tags=["compras"])
+# app.include_router(pagos.router, prefix="/pagos", tags=["pagos"])
+# app.include_router(pqrs.router, prefix="/pqrs", tags=["pqrs"])
 
 @app.get("/health")
 def health_check():
