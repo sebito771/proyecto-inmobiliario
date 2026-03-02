fcc3975bcb8e110ea5a6af4d0ac50410c9c835bc
Sebastian <sebascova18@gmail.com>
2026-03-01 22:22:05 -0500

    chore(repo): inicializa repositorios

---
+++ backend/app/repo/__init__.py
@@ -0,0 +1,5 @@
+from .lote import LoteRepository
+from .usuario import UsuarioRepository
+from .compra import CompraRepository
+from .detalle_compra import DetalleRepository
+from .etapa import EtapaRepository
