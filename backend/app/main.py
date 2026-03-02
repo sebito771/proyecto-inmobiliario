9d629a80148784ffd9d764cd75a80d49c3d3c137
Sebastian <sebascova18@gmail.com>
2026-03-02 01:49:55 -0500

    feat(app/main): añade router de usuario y pagos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import auth , lote, pqrs, rol, detalle_compra
+from app.api.routes import auth , lote, pqrs, rol, detalle_compra, pago, usuarios
 from dotenv import load_dotenv
 
 # ,usuarios, , compras, pagos, 
@@ -13,8 +13,9 @@ app.include_router(lote.router, prefix="/lotes", tags=["lotes"])
 app.include_router(pqrs.router, prefix="/pqrs", tags=["pqrs"])
 app.include_router(rol.router, prefix="/roles", tags=["roles"])
 app.include_router(detalle_compra.router, prefix="/detalle-compra", tags=["detalle_compra"])
+app.include_router(pago.router, prefix="/pagos", tags=["pagos"])
+app.include_router(usuarios.router, prefix="/usuarios", tags=["usuarios"])
 # app.include_router(compras.router, prefix="/compras", tags=["compras"])
-# app.include_router(pagos.router, prefix="/pagos", tags=["pagos"])
 
 @app.get("/health")
 def health_check():
