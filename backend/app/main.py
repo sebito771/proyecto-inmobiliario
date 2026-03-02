9613934b6a548fc1f300a5d537f89ecc815d0c19
Sebastian <sebascova18@gmail.com>
2026-03-02 00:48:24 -0500

    chore: añade al main los nuevos routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from app.api.routes import auth , lote
+from app.api.routes import auth , lote, pqrs, rol, detalle_compra
 from dotenv import load_dotenv
 
-# ,usuarios, , compras, pagos, pqrs
+# ,usuarios, , compras, pagos, 
 load_dotenv()
 app = FastAPI()
 #active
@@ -10,9 +10,11 @@ app.include_router(auth.router, prefix="/auth", tags=["auth"])
 #inactive
 #app.include_router(usuarios.router, prefix="/usuarios", tags=["usuarios"])
 app.include_router(lote.router, prefix="/lotes", tags=["lotes"])
+app.include_router(pqrs.router, prefix="/pqrs", tags=["pqrs"])
+app.include_router(rol.router, prefix="/roles", tags=["roles"])
+app.include_router(detalle_compra.router, prefix="/detalle-compra", tags=["detalle_compra"])
 # app.include_router(compras.router, prefix="/compras", tags=["compras"])
 # app.include_router(pagos.router, prefix="/pagos", tags=["pagos"])
-# app.include_router(pqrs.router, prefix="/pqrs", tags=["pqrs"])
 
 @app.get("/health")
 def health_check():
