5f806befa0f3cff07336c1747a66f765857977cf
Sebastian <sebascova18@gmail.com>
2026-03-02 00:46:09 -0500

    feat(api-dependencies): añade dependencias para los nuevos servicios

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
-from app.services import UsuarioServices , LoteServices
-from app.repo import UsuarioRepository , LoteRepository , CompraRepository , DetalleRepository , EtapaRepository
+from app.services import UsuarioServices , LoteServices , PqrsServices , RolServices , DetalleCompraServices
+from app.repo import UsuarioRepository , LoteRepository , CompraRepository , DetalleRepository , EtapaRepository , PqrsRepository , RolRepository
 
 
 
@@ -19,3 +19,21 @@ def get_lote_service(db: Session = Depends(get_db)) -> LoteServices:
     detalle_repo= DetalleRepository(db)
     etapa_repo= EtapaRepository(db)
     return LoteServices(repo,user_repo,compra_repo,detalle_repo,etapa_repo,db)
+
+
+def get_pqrs_service(db: Session = Depends(get_db)) -> PqrsServices:
+    repo = PqrsRepository(db)
+    user_repo = UsuarioRepository(db)
+    return PqrsServices(repo, user_repo)
+
+
+def get_rol_service(db: Session = Depends(get_db)) -> RolServices:
+    repo = RolRepository(db)
+    return RolServices(repo)
+
+
+def get_detalle_compra_service(db: Session = Depends(get_db)) -> DetalleCompraServices:
+    repo = DetalleRepository(db)
+    compra_repo = CompraRepository(db)
+    lote_repo = LoteRepository(db)
+    return DetalleCompraServices(repo, compra_repo, lote_repo)
